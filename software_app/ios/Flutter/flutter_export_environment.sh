a8952006b34212a06bb7aaca8e0eeb4d706e9d2b
e20367binuri <e20367@eng.pdn.ac.lk>
2025-05-11 12:53:01 +0530

    commit5

--- software_app/ios/Flutter/flutter_export_environment.sh
+++ software_app/ios/Flutter/flutter_export_environment.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\flutter"
+export "FLUTTER_ROOT=C:\Users\User\AppData\Local\flutter_windows_3.29.0-stable\flutter"
 export "FLUTTER_APPLICATION_PATH=E:\3yp\e20-3yp-VR-Multiplayer-Golf-Game\software_app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
